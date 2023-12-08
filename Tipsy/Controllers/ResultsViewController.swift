032a5ac1c5be776f3ed9de882ba50218a3f2086b
omergunek <gunekfaruk@hotmail.com>
2023-12-08 18:12:55 +0300

    first commit

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Ömer Günek on 5.12.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
